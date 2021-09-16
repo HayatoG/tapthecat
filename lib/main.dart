a36ed3c357fe4101d98188bb30b72276a6f8f424
guilherme <guilherme.ryders@gmail.com>
2021-09-16 15:27:55 -0300

    Adding first keyboard cat on home screen and adding the buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   runApp(TapTheCat());
@@ -16,16 +18,38 @@ class TapTheCat extends StatelessWidget {
 class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: RichText(
-            text: TextSpan(
-              text: 'Página Inicial',
-              style: DefaultTextStyle.of(context).style,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tap the Cat'),
+      ),
+      body: Column(
+        children: [
+          Image.network('https://assets.wallpapersin4k.org/uploads/2017/05/Keyboard-Cat-Wallpaper-24.jpg'),
+          Center(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: ElevatedButton(onPressed: () { },
+                    child: Text('Começar'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: ElevatedButton(onPressed: () { },
+                    child: Text('Opções'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: ElevatedButton(onPressed: () { },
+                    child: Text('Sobre'),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
+        ],
       ),
     );
   }
